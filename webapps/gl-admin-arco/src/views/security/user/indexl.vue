1d965f0b37586aece2054b956115222e330de8c0
diablo1303 <diabloposeidon@hotmail.com>
2023-06-06 15:30:48 +0800

    模型管理开发

---
+++ webapps/gl-admin-arco/src/views/security/user/indexl.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <!-- 面包屑 Breadcrumb 是辅助导航模式，用于识别页面在层次结构内的位置，并根据需要向上返回。-->
+    <Breadcrumb :items="['sercurity.user.index.menu.list', 'sercurity.user.index.menu.list.searchTable']"/>
+    <!-- 卡片 Card 将信息分类后分标题、详情等区域聚合展现，一般作为简洁介绍或者信息的大盘和入口。   -->
+    <a-card :title="$t('sercurity.user.index.menu.list.searchTable')" class="general-card">
+      <!-- list   -->
+      <UserList ref="userListRef"></UserList>
+    </a-card>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {onMounted, ref} from 'vue';
+import UserList from "@/views/security/user/list.vue";
+
+const userListRef = ref(null);
+
+const loadIndex = () => {
+  if (userListRef.value) {
+    userListRef.value?.loadList({action: 'edit'});
+  }
+}
+
+onMounted(() => {
+  loadIndex();
+});
+</script>
+
+<script lang="ts">
+export default {
+  name: 'SearchTable'
+};
+</script>
+
+<style lang="less" scoped>
+.container {
+  padding: 0 20px 20px 20px;
+}
+</style>
