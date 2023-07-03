df9a3a0e1183f47e70c8f38743f9783acad3196e
diablo1303 <diabloposeidon@hotmail.com>
2023-07-03 10:48:39 +0800

    隐藏顶部右侧菜单[搜索、语言切换、页面设置、用户功能]

--- webapps/gl-admin-arco/src/components/navbar/index.vue
+++ webapps/gl-admin-arco/src/components/navbar/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="navbar">
+    <!--  头部菜单，左侧菜单，应用图标、应用名称  -->
     <div class="left-side">
       <a-space>
         <span style="text-align: center">
@@ -10,41 +11,45 @@
           />
         </span>
         <a-typography-title
-          :style="{ margin: 0, fontSize: '18px' }"
-          :heading="5"
+            :style="{ margin: 0, fontSize: '18px' }"
+            :heading="5"
         >
           Geelato Admin Pro
         </a-typography-title>
         <icon-menu-fold
-          v-if="!topMenu && appStore.device === 'mobile'"
-          style="font-size: 22px; cursor: pointer"
-          @click="toggleDrawerMenu"
+            v-if="!topMenu && appStore.device === 'mobile'"
+            style="font-size: 22px; cursor: pointer"
+            @click="toggleDrawerMenu"
         />
       </a-space>
     </div>
+    <!--  头部菜单，中部菜单  -->
     <div class="center-side">
-      <Menu v-if="topMenu" />
+      <Menu v-if="topMenu"/>
     </div>
+    <!--  头部菜单，右侧菜单，搜索、语言切换、明暗背景、消息、全屏模式、页面设置、用户  -->
     <ul class="right-side">
-      <li>
+      <!--   搜索   -->
+      <li v-show="false">
         <a-tooltip :content="$t('settings.search')">
           <a-button class="nav-btn" type="outline" :shape="'circle'">
             <template #icon>
-              <icon-search />
+              <icon-search/>
             </template>
           </a-button>
         </a-tooltip>
       </li>
-      <li>
+      <!--   语言切换   -->
+      <li v-show="false">
         <a-tooltip :content="$t('settings.language')">
           <a-button
-            class="nav-btn"
-            type="outline"
-            :shape="'circle'"
-            @click="setDropDownVisible"
+              class="nav-btn"
+              type="outline"
+              :shape="'circle'"
+              @click="setDropDownVisible"
           >
             <template #icon>
-              <icon-language />
+              <icon-language/>
             </template>
           </a-button>
         </a-tooltip>
@@ -52,137 +57,146 @@
           <div ref="triggerBtn" class="trigger-btn"></div>
           <template #content>
             <a-doption
-              v-for="item in locales"
-              :key="item.value"
-              :value="item.value"
+                v-for="item in locales"
+                :key="item.value"
+                :value="item.value"
             >
               <template #icon>
-                <icon-check v-show="item.value === currentLocale" />
+                <icon-check v-show="item.value === currentLocale"/>
               </template>
               {{ item.label }}
             </a-doption>
           </template>
         </a-dropdown>
       </li>
+      <!--   明暗背景   -->
       <li>
         <a-tooltip
-          :content="
+            :content="
             theme === 'light'
               ? $t('settings.navbar.theme.toDark')
               : $t('settings.navbar.theme.toLight')
           "
         >
           <a-button
-            class="nav-btn"
-            type="outline"
-            :shape="'circle'"
-            @click="handleToggleTheme"
+              class="nav-btn"
+              type="outline"
+              :shape="'circle'"
+              @click="handleToggleTheme"
           >
             <template #icon>
-              <icon-moon-fill v-if="theme === 'dark'" />
-              <icon-sun-fill v-else />
+              <icon-moon-fill v-if="theme === 'dark'"/>
+              <icon-sun-fill v-else/>
             </template>
           </a-button>
         </a-tooltip>
       </li>
+      <!--   消息   -->
       <li>
         <a-tooltip :content="$t('settings.navbar.alerts')">
           <div class="message-box-trigger">
             <a-badge :count="9" dot>
               <a-button
-                class="nav-btn"
-                type="outline"
-                :shape="'circle'"
-                @click="setPopoverVisible"
+                  class="nav-btn"
+                  type="outline"
+                  :shape="'circle'"
+                  @click="setPopoverVisible"
               >
-                <icon-notification />
+                <icon-notification/>
               </a-button>
             </a-badge>
           </div>
         </a-tooltip>
         <a-popover
-          trigger="click"
-          :arrow-style="{ display: 'none' }"
-          :content-style="{ padding: 0, minWidth: '400px' }"
-          content-class="message-popover"
+            trigger="click"
+            :arrow-style="{ display: 'none' }"
+            :content-style="{ padding: 0, minWidth: '400px' }"
+            content-class="message-popover"
         >
           <div ref="refBtn" class="ref-btn"></div>
           <template #content>
-            <message-box />
+            <message-box/>
           </template>
         </a-popover>
       </li>
+      <!--   全屏模式   -->
       <li>
         <a-tooltip
-          :content="
+            :content="
             isFullscreen
               ? $t('settings.navbar.screen.toExit')
               : $t('settings.navbar.screen.toFull')
           "
         >
           <a-button
-            class="nav-btn"
-            type="outline"
-            :shape="'circle'"
-            @click="toggleFullScreen"
+              class="nav-btn"
+              type="outline"
+              :shape="'circle'"
+              @click="toggleFullScreen"
           >
             <template #icon>
-              <icon-fullscreen-exit v-if="isFullscreen" />
-              <icon-fullscreen v-else />
+              <icon-fullscreen-exit v-if="isFullscreen"/>
+              <icon-fullscreen v-else/>
             </template>
           </a-button>
         </a-tooltip>
       </li>
-      <li>
+      <!--  页面设置    -->
+      <li v-show="false">
         <a-tooltip :content="$t('settings.title')">
           <a-button
-            class="nav-btn"
-            type="outline"
-            :shape="'circle'"
-            @click="setVisible"
+              class="nav-btn"
+              type="outline"
+              :shape="'circle'"
+              @click="setVisible"
           >
             <template #icon>
-              <icon-settings />
+              <icon-settings/>
             </template>
           </a-button>
         </a-tooltip>
       </li>
+      <!--   用户   -->
       <li>
         <a-dropdown trigger="click">
           <a-avatar
-            :size="32"
-            :style="{ marginRight: '8px', cursor: 'pointer' }"
+              :size="32"
+              :style="{ marginRight: '8px', cursor: 'pointer' }"
           >
-            <img alt="avatar" :src="avatar" />
+            <img alt="avatar" :src="avatar"/>
           </a-avatar>
           <template #content>
-            <a-doption>
+            <!--     切换角色       -->
+            <a-doption v-show="false">
               <a-space @click="switchRoles">
-                <icon-tag />
+                <icon-tag/>
                 <span>
                   {{ $t('messageBox.switchRoles') }}
                 </span>
               </a-space>
             </a-doption>
-            <a-doption>
+            <!--     用户中心       -->
+            <a-doption v-show="false">
               <a-space @click="$router.push({ name: 'Info' })">
-                <icon-user />
+                <icon-user/>
                 <span>
                   {{ $t('messageBox.userCenter') }}
                 </span>
               </a-space>
             </a-doption>
-            <a-doption>
+            <!--     用户设置       -->
+            <a-doption v-show="false">
               <a-space @click="$router.push({ name: 'Setting' })">
-                <icon-settings />
+                <icon-settings/>
                 <span>
                   {{ $t('messageBox.userSettings') }}
                 </span>
               </a-space>
             </a-doption>
+            <!--      登出登录      -->
             <a-doption>
               <a-space @click="handleLogout">
-                <icon-export />
+                <icon-export/>
                 <span>
                   {{ $t('messageBox.logout') }}
                 </span>
@@ -196,131 +210,136 @@
 </template>
 
 <script lang="ts" setup>
-  import { computed, ref, inject } from 'vue';
-  import { Message } from '@arco-design/web-vue';
-  import { useDark, useToggle, useFullscreen } from '@vueuse/core';
-  import { useAppStore, useUserStore } from '@/store';
-  import { LOCALE_OPTIONS } from '@/locale';
-  import useLocale from '@/hooks/locale';
-  import useUser from '@/hooks/user';
-  import Menu from '@/components/menu/index.vue';
-  import MessageBox from '../message-box/index.vue';
+import {computed, inject, ref} from 'vue';
+import {Message} from '@arco-design/web-vue';
+import {useDark, useFullscreen, useToggle} from '@vueuse/core';
+import {useAppStore, useUserStore} from '@/store';
+import {LOCALE_OPTIONS} from '@/locale';
+import useLocale from '@/hooks/locale';
+import useUser from '@/hooks/user';
+import Menu from '@/components/menu/index.vue';
+import MessageBox from '../message-box/index.vue';
 
-  const appStore = useAppStore();
-  const userStore = useUserStore();
-  const { logout } = useUser();
-  const { changeLocale, currentLocale } = useLocale();
-  const { isFullscreen, toggle: toggleFullScreen } = useFullscreen();
-  const locales = [...LOCALE_OPTIONS];
-  const avatar = computed(() => {
-    return userStore.avatar;
+const appStore = useAppStore();
+const userStore = useUserStore();
+const {logout} = useUser();
+const {changeLocale, currentLocale} = useLocale();
+const {isFullscreen, toggle: toggleFullScreen} = useFullscreen();
+const locales = [...LOCALE_OPTIONS];
+const avatar = computed(() => {
+  return userStore.avatar;
+});
+const theme = computed(() => {
+  return appStore.theme;
+});
+const topMenu = computed(() => appStore.topMenu && appStore.menu);
+const isDark = useDark({
+  selector: 'body',
+  attribute: 'arco-theme',
+  valueDark: 'dark',
+  valueLight: 'light',
+  storageKey: 'arco-theme',
+  onChanged(dark: boolean) {
+    // overridden default behavior
+    appStore.toggleTheme(dark);
+  },
+});
+const toggleTheme = useToggle(isDark);
+const handleToggleTheme = () => {
+  toggleTheme();
+};
+const setVisible = () => {
+  appStore.updateSettings({globalSettings: true});
+};
+const refBtn = ref();
+const triggerBtn = ref();
+const setPopoverVisible = () => {
+  const event = new MouseEvent('click', {
+    view: window,
+    bubbles: true,
+    cancelable: true,
   });
-  const theme = computed(() => {
-    return appStore.theme;
+  refBtn.value.dispatchEvent(event);
+};
+const handleLogout = () => {
+  logout();
+};
+const setDropDownVisible = () => {
+  const event = new MouseEvent('click', {
+    view: window,
+    bubbles: true,
+    cancelable: true,
   });
-  const topMenu = computed(() => appStore.topMenu && appStore.menu);
-  const isDark = useDark({
-    selector: 'body',
-    attribute: 'arco-theme',
-    valueDark: 'dark',
-    valueLight: 'light',
-    storageKey: 'arco-theme',
-    onChanged(dark: boolean) {
-      // overridden default behavior
-      appStore.toggleTheme(dark);
-    },
-  });
-  const toggleTheme = useToggle(isDark);
-  const handleToggleTheme = () => {
-    toggleTheme();
-  };
-  const setVisible = () => {
-    appStore.updateSettings({ globalSettings: true });
-  };
-  const refBtn = ref();
-  const triggerBtn = ref();
-  const setPopoverVisible = () => {
-    const event = new MouseEvent('click', {
-      view: window,
-      bubbles: true,
-      cancelable: true,
-    });
-    refBtn.value.dispatchEvent(event);
-  };
-  const handleLogout = () => {
-    logout();
-  };
-  const setDropDownVisible = () => {
-    const event = new MouseEvent('click', {
-      view: window,
-      bubbles: true,
-      cancelable: true,
-    });
-    triggerBtn.value.dispatchEvent(event);
-  };
-  const switchRoles = async () => {
-    const res = await userStore.switchRoles();
-    Message.success(res as string);
-  };
-  const toggleDrawerMenu = inject('toggleDrawerMenu') as () => void;
+  triggerBtn.value.dispatchEvent(event);
+};
+const switchRoles = async () => {
+  const res = await userStore.switchRoles();
+  Message.success(res as string);
+};
+const toggleDrawerMenu = inject('toggleDrawerMenu') as () => void;
 </script>
 
 <style scoped lang="less">
-  .navbar {
-    display: flex;
-    justify-content: space-between;
-    height: 100%;
-    background-color: var(--color-bg-2);
-    border-bottom: 1px solid var(--color-border);
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  height: 100%;
+  background-color: var(--color-bg-2);
+  border-bottom: 1px solid var(--color-border);
+}
+
+.left-side {
+  display: flex;
+  align-items: center;
+  padding-left: 20px;
+}
+
+.center-side {
+  flex: 1;
+}
+
+.right-side {
+  display: flex;
+  padding-right: 20px;
+  list-style: none;
+
+  :deep(.locale-select) {
+    border-radius: 20px;
   }
 
-  .left-side {
+  li {
     display: flex;
     align-items: center;
-    padding-left: 20px;
+    padding: 0 10px;
   }
 
-  .center-side {
-    flex: 1;
+  a {
+    color: var(--color-text-1);
+    text-decoration: none;
   }
 
-  .right-side {
-    display: flex;
-    padding-right: 20px;
-    list-style: none;
-    :deep(.locale-select) {
-      border-radius: 20px;
-    }
-    li {
-      display: flex;
-      align-items: center;
-      padding: 0 10px;
-    }
-
-    a {
-      color: var(--color-text-1);
-      text-decoration: none;
-    }
-    .nav-btn {
-      border-color: rgb(var(--gray-2));
-      color: rgb(var(--gray-8));
-      font-size: 16px;
-    }
-    .trigger-btn,
-    .ref-btn {
-      position: absolute;
-      bottom: 14px;
-    }
-    .trigger-btn {
-      margin-left: 14px;
-    }
+  .nav-btn {
+    border-color: rgb(var(--gray-2));
+    color: rgb(var(--gray-8));
+    font-size: 16px;
   }
+
+  .trigger-btn,
+  .ref-btn {
+    position: absolute;
+    bottom: 14px;
+  }
+
+  .trigger-btn {
+    margin-left: 14px;
+  }
+}
 </style>
 
 <style lang="less">
-  .message-popover {
-    .arco-popover-content {
-      margin-top: 0;
-    }
+.message-popover {
+  .arco-popover-content {
+    margin-top: 0;
   }
+}
 </style>
