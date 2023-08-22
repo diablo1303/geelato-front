c74a3e03117d34b8e5bb250cb1c5be5fd79e10e7
geelato <geelato@126.com>
2023-08-22 23:40:06 +0800

    调整相关文件的加载顺序和开发保持一致

--- webapps/gl-admin-webapp-arco/src/main.ts
+++ webapps/gl-admin-webapp-arco/src/main.ts
@@ -2,23 +2,23 @@ import {createApp} from 'vue';
 import ArcoVue from '@arco-design/web-vue';
 import ArcoVueIcon from '@arco-design/web-vue/es/icon';
 import globalComponents from '@/components';
-import geelatoMain from "@/main.geelato";
-import router from './router';
 import store from './store';
+import router from './router';
 import i18n from './locale';
 import directive from './directive';
 import App from './App.vue';
+import '@/api/interceptor';
 import '@arco-design/web-vue/dist/arco.css';
 import '@/assets/style/global.less';
-import '@/api/interceptor';
+import geelatoMain from "./main.geelato";
 
 const app = createApp(App);
 
 app.use(ArcoVue, {});
 app.use(ArcoVueIcon);
 
-app.use(router);
 app.use(store);
+app.use(router);
 app.use(i18n);
 app.use(globalComponents);
 app.use(directive);
