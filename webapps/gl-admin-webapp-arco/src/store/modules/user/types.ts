95678d5086685e19501af95e2d6404c130e753d5
diablo1303 <diabloposeidon@hotmail.com>
2023-09-15 10:39:38 +0800

    右上角添加登录用户名称和账户

--- webapps/gl-admin-webapp-arco/src/store/modules/user/types.ts
+++ webapps/gl-admin-webapp-arco/src/store/modules/user/types.ts
@@ -1,21 +1,28 @@
 export type RoleType = '' | '*' | 'admin' | 'user';
 
 export interface UserState {
-  name?: string;
-  avatar?: string;
-  job?: string;
-  organization?: string;
-  location?: string;
-  email?: string;
-  introduction?: string;
-  personalWebsite?: string;
-  jobName?: string;
-  organizationName?: string;
-  locationName?: string;
-  phone?: string;
-  registrationDate?: string;
-  accountId?: string;
-  certification?: number;
-  tenantCode?: string;
-  role: RoleType;
+  id?: string; // 主键
+  name?: string; // 名称
+  loginName?: string; // 登录名称
+  avatar?: string; // 头像
+  mobilePrefix?: string; // 手机区域编号
+  mobilePhone?: string; // 手机号码
+  email?: string; // 邮箱
+  registrationDate?: string; // 注册时间
+  personalWebsite?: string; //
+  introduction?: string; //
+  certification?: number; //
+  job?: string; //
+  jobName?: string; //
+  location?: string; //
+  locationName?: string; //
+  orgId?: string; // 部门ID
+  orgName?: string; // 部门名称
+  description?: string; // 简介
+  nationCode?: string; // 详细地址
+  provinceCode?: string; // 详细地址
+  cityCode?: string; // 详细地址
+  address?: string; // 详细地址
+  tenantCode?: string; // 租户
+  role: RoleType; // 角色
 }
