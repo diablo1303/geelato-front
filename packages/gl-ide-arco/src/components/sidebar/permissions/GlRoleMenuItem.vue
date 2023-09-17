b3e515da2761bfaf03d79acc4e94e3c7176aff9d
geelato <geelato@126.com>
2023-09-17 18:19:24 +0800

    Update GlRoleMenuItem.vue

--- packages/gl-ide-arco/src/components/sidebar/permissions/GlRoleMenuItem.vue
+++ packages/gl-ide-arco/src/components/sidebar/permissions/GlRoleMenuItem.vue
@@ -4,30 +4,29 @@ export default {
 }
 </script>
 <script lang="ts" setup>
-
-import {type Ref, ref, watch} from "vue";
-import {useAppStore} from "@geelato/gl-ide";
-import {entityApi, utils} from "@geelato/gl-ui";
+import { type Ref, ref, watch } from 'vue'
+import { useAppStore } from '@geelato/gl-ide'
+import { entityApi, utils } from '@geelato/gl-ui'
 
 interface RoleGrantedMenuItem {
-  id: string,
-  treeNodeId: string,
+  id: string
+  treeNodeId: string
   roleId: string
 }
 
 // id key,pid,text name,iconType
 interface AppMenuItem {
-  key: string,
-  pid: string,
-  name: string,
-  iconType: string,
-  checked: boolean,
+  key: string
+  pid: string
+  name: string
+  iconType: string
+  checked: boolean
   children: AppMenuItem[]
 }
 
 interface Role {
-  id: string,
-  name: string,
+  id: string
+  name: string
   code: string
 }
 
@@ -56,23 +55,23 @@ const appRoles: Ref<Role[]> = ref([])
 const appMenuItems: Ref<AppMenuItem[]> = ref([])
 // 当前选择角色已授权菜单信息
 const currentRoleGrantedMenuItems: Ref<RoleGrantedMenuItem[]> = ref([])
-// 当前正在配置的应和菜单，包括当前的应用菜单信息、授权的菜单信息
+// 树结构数据，当前正在配置的应和菜单，包括当前的应用菜单信息、授权的菜单信息
 const currentRoleAppMenuItems: Ref<AppMenuItem[]> = ref([])
 
 // 当前应用的角色
 const loadAppRoles = (appId: string) => {
   // TODO 待增加应用id条件
-  entityApi.query('platform_role', 'id,name,code',
-      {'@p': '1,1000'}).then((res) => {
+  entityApi.query('platform_role', 'id,name,code', { '@p': '1,1000' }).then((res) => {
     appRoles.value = res.data
   })
 }
 
 const loadAppMenuItems = (appId: string) => {
-  entityApi.query('platform_tree_node', 'id key,pid,text name,iconType',
-      {'@p': '1,2000', 'treeId': appId}).then((res) => {
-    appMenuItems.value = utils.listToTree(res.data, appId, {id: 'key'})
-  })
+  entityApi
+    .query('platform_tree_node', 'id key,pid,text name,iconType', { '@p': '1,2000', treeId: appId })
+    .then((res) => {
+      appMenuItems.value = utils.listToTree(res.data, appId, { id: 'key' })
+    })
 }
 
 /**
@@ -84,7 +83,6 @@ const setTreeItemChecked = (menuItems: AppMenuItem[], treeNodeId: string) => {
   // console.log('setTreeItemChecked:', menuItems, treeNodeId)
   menuItems?.forEach((item) => {
     if (item.key === treeNodeId) {
-
       item.checked = true
     }
     setTreeItemChecked(item.children, treeNodeId)
@@ -95,16 +93,21 @@ const setTreeItemChecked = (menuItems: AppMenuItem[], treeNodeId: string) => {
  *  加载某角色已授权的菜单
  */
 const loadRoleGrantedMenuItem = (appId: string, roleId: string) => {
-  entityApi.query('platform_role_r_tree_node', 'id,treeNodeId key,roleId',
-      {'treeId': appId, roleId: roleId}).then((res) => {
-    currentRoleGrantedMenuItems.value = res.data
-    // 重置当前的角色菜单
-    const appMenuItemsCopy: AppMenuItem[] = JSON.parse(JSON.stringify(appMenuItems.value))
-    currentRoleGrantedMenuItems.value?.forEach((item: RoleGrantedMenuItem) => {
-      setTreeItemChecked(appMenuItemsCopy, item.treeNodeId)
+  entityApi
+    .query('platform_role_r_tree_node', 'id,treeNodeId key,roleId', {
+      treeId: appId,
+      roleId: roleId
     })
-    currentRoleAppMenuItems.value = appMenuItemsCopy
-  })
+    .then((res) => {
+      // 从服务端获取已有权限
+      currentRoleGrantedMenuItems.value = res.data
+      // 重置当前的角色菜单
+      const appMenuItemsCopy: AppMenuItem[] = JSON.parse(JSON.stringify(appMenuItems.value))
+      currentRoleGrantedMenuItems.value?.forEach((item: RoleGrantedMenuItem) => {
+        setTreeItemChecked(appMenuItemsCopy, item.treeNodeId)
+      })
+      currentRoleAppMenuItems.value = appMenuItemsCopy
+    })
 }
 
 const selectAllMenuItem = () => {
@@ -142,39 +145,31 @@ const selectRole = (role: any, roleIndex: number) => {
 }
 
 const save = () => {
-  console.log('currentRoleAppMenuItems', currentRoleAppMenuItems.value)
-
   // 待删除的授权信息 toDeleteItems
   const toDeleteItems: RoleGrantedMenuItem[] = []
 
-  const findDeleteItem = (menuItems: AppMenuItem[], grantedItem: RoleGrantedMenuItem) => {
-    let foundDeleteItem = undefined
-    menuItems?.forEach((item: AppMenuItem) => {
-      if (item.key === grantedItem.treeNodeId && !item.checked) {
-        foundDeleteItem = grantedItem
-      } else {
-        foundDeleteItem = findDeleteItem(item.children, grantedItem)
-      }
-    })
-    return foundDeleteItem
-  }
-
+  // 当前角色可配置授权的菜单
+  const currentRoleAppMenuItemMap:Record<string, any> = {}
+  utils.treeToMap(currentRoleAppMenuItems.value, 'key', currentRoleAppMenuItemMap)
   currentRoleGrantedMenuItems.value.forEach((grantedItem: RoleGrantedMenuItem) => {
-    const item = findDeleteItem(currentRoleAppMenuItems.value, grantedItem)
-    if (item) {
+    const appMenuItem = currentRoleAppMenuItemMap[grantedItem.treeNodeId]
+    // 已有的授权哪些已取消
+    if (appMenuItem && !appMenuItem.checked) {
       toDeleteItems.push(grantedItem)
     }
   })
 
-
   // 待添加的授权信息 toAddItems
   const toAddItems: any[] = []
-  const addItem = (menuItems: AppMenuItem[]) => {
-    menuItems.forEach((item: any) => {
+  const genAddItems = () => {
+    Object.keys(currentRoleAppMenuItemMap).forEach((key: string) => {
+      const item = currentRoleAppMenuItemMap[key]
       if (item.checked) {
-        const foundNotToAddItem = currentRoleGrantedMenuItems.value.find((grantedItem: RoleGrantedMenuItem) => {
-          return grantedItem.treeNodeId === item.key
-        })
+        const foundNotToAddItem = currentRoleGrantedMenuItems.value.find(
+          (grantedItem: RoleGrantedMenuItem) => {
+            return grantedItem.treeNodeId === item.key
+          }
+        )
         if (!foundNotToAddItem) {
           toAddItems.push({
             treeId: appId,
@@ -186,31 +181,39 @@ const save = () => {
           })
         }
       }
-      if (item.children && item.children.length > 0) {
-        addItem(item.children)
-      }
     })
   }
-  addItem(currentRoleAppMenuItems.value)
+  genAddItems()
 
   console.log('toAddItems', toAddItems, 'toDeleteItems', toDeleteItems)
 
-  // 需放到事务中
+  let promises: Promise<any>[] = []
+  // TODO 需放到事务中
   toDeleteItems.forEach((item) => {
-    entityApi.delete('platform_role_r_tree_node', {id: item.id})
-
+    const promise = entityApi.delete('platform_role_r_tree_node', { id: item.id })
+    promises.push(promise)
   })
 
   toAddItems.forEach((item) => {
-    entityApi.save('platform_role_r_tree_node', item)
+    const promise = entityApi.save('platform_role_r_tree_node', item)
+    promises.push(promise)
   })
 
+  Promise.all(promises).then((values) => {
+    // 保存之后需刷新
+    reset()
+  })
 }
 
+const reset = () => {
+  loadAppRoles(appId)
+  loadAppMenuItems(appId)
+  loadRoleGrantedMenuItem(appId, currentRole.value.id)
+}
 
-loadAppRoles(appId)
-loadAppMenuItems(appId)
-defineExpose({save})
+reset()
+
+defineExpose({ save })
 </script>
 
 <template>
@@ -222,17 +225,23 @@ defineExpose({save})
             <span class="gl-title">{{ appStore.currentApp.name }}角色</span>
           </div>
         </template>
-        <a-list-item v-for="(role,roleIndex) in appRoles" :class="{'gl-active':roleIndex===currentRoleIndex}"
-                     @click="selectRole(role,roleIndex)">
+        <a-list-item
+          v-for="(role, roleIndex) in appRoles"
+          :class="{ 'gl-active': roleIndex === currentRoleIndex }"
+          @click="selectRole(role, roleIndex)"
+        >
           <span>{{ role.name }}</span>
         </a-list-item>
       </a-list>
     </div>
     <div class="gl-layout-right">
-      <a-table :data="currentRoleAppMenuItems" v-model:expandedKeys="expandedKeys"
-               stripe style="margin-left: 20px"
-               :bordered="{wrapper: true, cell: true}"
-               :pagination="false"
+      <a-table
+        :data="currentRoleAppMenuItems"
+        v-model:expandedKeys="expandedKeys"
+        stripe
+        style="margin-left: 20px"
+        :bordered="{ wrapper: true, cell: true }"
+        :pagination="false"
       >
         <template #columns>
           <a-table-column title="菜单项" data-index="name">
@@ -250,13 +259,11 @@ defineExpose({save})
               </a-button-group>
             </template>
             <template #cell="{ record }">
-              <a-switch v-model="record.checked"/>
+              <a-switch v-model="record.checked" />
             </template>
           </a-table-column>
           <a-table-column title="该页面相关的模型" data-index="entities">
-            <template #cell="{ record }">
-
-            </template>
+            <template #cell="{ record }"></template>
           </a-table-column>
         </template>
       </a-table>
@@ -276,7 +283,6 @@ defineExpose({save})
 
 .gl-role-menuitem .arco-list-header {
   background-color: #efefef;
-
 }
 
 .gl-role-menuitem .gl-layout-left {
