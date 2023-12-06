de79c814b84536ee53bf2a76f4401d09819fd5ba
geelato <geelato@126.com>
2023-12-06 15:05:11 +0800

    更改alert

--- packages/gl-ui-arco/src/components/gl-alert/GlAlert.vue
+++ packages/gl-ui-arco/src/components/gl-alert/GlAlert.vue
@@ -5,12 +5,16 @@ export default {
 </script>
 <script lang="ts" setup>
 const props = defineProps({
+  label: String,
   _content: String
 })
 </script>
 
 <template>
   <a-alert>
+    <template #title v-if="label">
+      {{ label }}
+    </template>
     {{ _content }}
     <template #action>
       <slot name="action"></slot>
