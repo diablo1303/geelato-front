06e298da6f13f0802ec014d729e846ee58cba267
geelato <geelato@126.com>
2024-03-19 23:22:00 +0800

    调整组件目录等

---
+++ packages/gl-ui-arco/src/components-standalone/gl-alert-sd/index.ts
@@ -0,0 +1,11 @@
+import index from './index.vue'
+export default index
+// import type {App} from 'vue'
+// import index from './index.vue'
+//
+// export default Object.assign(index, {
+//     install: (app: App, options?: Record<string, any>) => {
+//         console.log('全局注册组件', index.name)
+//         app.component(index.name, index)
+//     }
+// })
