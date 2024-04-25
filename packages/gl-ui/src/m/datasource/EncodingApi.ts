34dcc4658597d8af79df9db10e828a0b0ce77181
diablo1303 <diabloposeidon@hotmail.com>
2024-04-25 14:57:12 +0800

    编码生成接口修正

--- packages/gl-ui/src/m/datasource/EncodingApi.ts
+++ packages/gl-ui/src/m/datasource/EncodingApi.ts
@@ -1,10 +1,13 @@
 import {entityApi} from "./EntityApi";
-
+import utils from '../utils/Utils';
 
 /**
- * 基于编码id获取一个新的编码
- * @param id
+ * 获取一个新的编码，如需要使用系统编码app，需在参数中传入appId
+ * @param id 编码ID
+ * @param argumentParam params参数
+ * @param argumentBody body参数，优先级高于params参数
  */
-export function generateCode(id: string) {
-    return entityApi.getAxios().post(`/api/encoding/generate/${id}`);
+export function generateCode(id: string, argumentParam?: Record<string, any>, argumentBody?: Record<string, any>) {
+  const records = utils.getUrlParams(argumentParam || {});
+  return entityApi.getAxios().post(`/api/encoding/generate/${id}?${records.join('&')}`, argumentBody || {});
 }
