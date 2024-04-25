fbda29c0e02c9e522562610942ae14782ce9f7a5
geelato <geelato@126.com>
2024-04-25 13:25:19 +0800

    Update EncodingApi.ts

--- packages/gl-ui/src/m/datasource/EncodingApi.ts
+++ packages/gl-ui/src/m/datasource/EncodingApi.ts
@@ -1,10 +1,10 @@
-import {entityApi} from "./EntityApi";
-
+import { entityApi } from './EntityApi'
 
 /**
  * 基于编码id获取一个新的编码
  * @param id
+ * @param params 编码变量，如可传一些业务类型代码，{type:'ap'}、{type:'ar'}、
  */
-export function generateCode(id: string) {
-    return entityApi.getAxios().post(`/api/encoding/generate/${id}`);
+export function generateCode(id: string, params?: Record<string, string>) {
+  return entityApi.getAxios().post(`/api/encoding/generate/${id}`, params || {})
 }
