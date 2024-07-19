8291b0cc44e28cd168286b135ba49b4069851976
geelato <geelato@126.com>
2024-07-19 11:50:59 +0800

    Update vite.config.test.ts

--- webapps/gl-admin-arco/config/vite.config.test.ts
+++ webapps/gl-admin-arco/config/vite.config.test.ts
@@ -1,9 +1,9 @@
 import {mergeConfig} from 'vite';
-// import topLevelAwait from 'vite-plugin-top-level-await';
+import topLevelAwait from 'vite-plugin-top-level-await';
 import baseConfig from './vite.config.base';
 import configCompressPlugin from './plugin/compress';
 import configVisualizerPlugin from './plugin/visualizer';
-// import configArcoResolverPlugin from './plugin/arcoResolver';
+import configArcoResolverPlugin from './plugin/arcoResolver';
 import configStyleImportPlugin from './plugin/styleImport';
 import configImageminPlugin from './plugin/imagemin';
 
@@ -16,10 +16,10 @@ export default mergeConfig(
             // configArcoResolverPlugin(),
             configStyleImportPlugin(),
             configImageminPlugin(),
-            // topLevelAwait({
-            //     promiseExportName: '__tla',
-            //     promiseImportName: i => `__tla_${i}`
-            // })
+            topLevelAwait({
+                promiseExportName: '__tla',
+                promiseImportName: i => `__tla_${i}`
+            })
         ],
         build: {
             rollupOptions: {
