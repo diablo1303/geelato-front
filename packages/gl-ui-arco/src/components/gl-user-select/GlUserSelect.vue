6d0063def768796ca48368b7aed049899eec4b4a
geelato <geelato@126.com>
2024-08-13 12:06:52 +0800

    用户组件，支持过滤用户类型范围

--- packages/gl-ui-arco/src/components/gl-user-select/GlUserSelect.vue
+++ packages/gl-ui-arco/src/components/gl-user-select/GlUserSelect.vue
@@ -5,14 +5,14 @@ export default {
 </script>
 
 <script setup lang="ts">
-import { computed, inject, ref, watch } from 'vue'
+import { computed, inject, type PropType, ref, watch } from 'vue'
 import {
   entityApi,
   EntityReader,
   EntityReaderParam,
   FieldMeta,
   PageProvideKey,
-  PageProvideProxy
+  PageProvideProxy, utils
 } from '@geelato/gl-ui'
 
 const pageProvideProxy: PageProvideProxy = inject(PageProvideKey)!
@@ -59,6 +59,18 @@ const props = defineProps({
     type: String,
     default() {
       return ''
+    }
+  },
+  /**
+   *  0；内部账号
+   *  1、外部账号
+   *  2、系统账号
+   *  默认为内部账号
+   */
+  userTypes:{
+    type: Array as PropType<Array<string>>,
+    default(){
+      return ['0']
     }
   }
 })
@@ -125,6 +137,13 @@ const loadUserDataItems = (ids?: Array<any>) => {
     } else {
       reader.params.push(new EntityReaderParam('id', 'in', ids))
     }
+  }
+  // 去掉空值，因存在[""]的情况
+  const types = props.userTypes.filter((item: any) => {
+    return !utils.isEmpty(item)
+  })
+  if(types&&types.length > 0) {
+    reader.params.push(new EntityReaderParam('type', 'in', types))
   }
   return entityApi.queryByEntityReader(reader)
 }
